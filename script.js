4446758fdd56ed685d3fbc0b6559c2c6d315d11e
Rubén <166368248+RubenSM9@users.noreply.github.com>
2025-03-18 13:04:43 +0100

    Comit con cambios de David

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Resaltar las secciones al pasar el ratón
 document.querySelectorAll('section').forEach(section => {
-    section.addEventListener('mouseenter', () => {
-      section.style.backgroundColor = '#e1e1e1';
-    });
-  
-    section.addEventListener('mouseleave', () => {
-      section.style.backgroundColor = '';
-    });
+  section.addEventListener('mouseenter', () => {
+    section.style.backgroundColor = '#e1e1e1';
   });
-  
+
+  section.addEventListener('mouseleave', () => {
+    section.style.backgroundColor = '';
+  });
+});
